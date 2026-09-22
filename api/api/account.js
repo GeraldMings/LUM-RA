24c0714fea5c78d01f8d55c1eb2a524f354bb938
Gerald Mingle <geemingle15@gmail.com>
2026-09-22 13:57:19 +0000

    Fix authorized parties in account.js

--- api/api/account.js
+++ api/api/account.js
@@ -14,9 +14,9 @@ export default async function handler(req, res) {
     const { isAuthenticated, toAuth } =
       await clerkClient.authenticateRequest(req, {
         authorizedParties: [
-          "https://lum-ra.vercel.app",
-          "https://lum-d7ipe8e2b-geraldmings.vercel.app"
-        ]
+  "https://lum-ra.vercel.app"
+]
+  
       });
 
     if (!isAuthenticated) {
